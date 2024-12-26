17b30e20cd7143cb13ea0c4dfc10a5c6f56764a2
Nghia <20130334@st.hcmuaf.edu.vn>
2024-12-26 23:46:51 +0700

    điều chỉnh lỗi màn hình user và chỉnh lại giao diện footer

--- lib/base_screen.dart
+++ lib/base_screen.dart
@@ -1,23 +1,52 @@
-import 'package:book_app/widgets/footer.dart';
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/chapter_screen.dart';
+import 'screens/read_screen.dart';
+import 'screens/user_screen.dart';
+import 'widgets/footer.dart';
 
-class BaseScreen extends StatelessWidget {
-  final Widget child;
-  final AppBar? appBar;
+class BaseScreen extends StatefulWidget {
   final int selectedIndex;
 
-  const BaseScreen({
-    required this.child,
-    this.appBar,
-    required this.selectedIndex,
-  });
+  const BaseScreen({super.key, this.selectedIndex = 0});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    ChapterScreen(),
+    ReadScreen(),
+    UserScreen(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.selectedIndex;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar,
-      body: child,
-      bottomNavigationBar: Footer(selectedIndex: selectedIndex),  // Sử dụng Footer widget
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: Footer(
+        selectedIndex: _currentIndex,
+        onItemTapped: _onItemTapped,
+      ),
     );
   }
 }
